67c2eba08b927c672d1148fd405bf81d07afa199
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-06-26 15:55:57 +0700

    advance css

---
+++ index.ts
@@ -0,0 +1,19 @@
+let num: number = 10
+let str: string = "abc"
+
+let arrNum: number[] = []
+
+type IPerson = {
+    name: string,
+    age: number
+}
+
+let person: IPerson = {
+    name: "andi",
+    age: 23
+}
+
+let person2: IPerson = {
+    name: "budi",
+    age: 20
+}
